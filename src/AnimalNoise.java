94424fdfc6a10ced8f01a65bc6820ee3c2b79bec
Kelli K <kjkleindorfer@dmacc.edu>
2020-01-23 09:41:21 -0600

    Update AnimalNoise.java

--- src/AnimalNoise.java
+++ src/AnimalNoise.java
@@ -1,12 +1,8 @@
-
-//change in browser 
 import model.*;
 
 public class AnimalNoise {
 	//add your animal class to the model package
 	//only push this file and the animal class you created
-	
-	// Test
 
 	public static void main(String[] args) {
 		Cat fluffy = new Cat();
